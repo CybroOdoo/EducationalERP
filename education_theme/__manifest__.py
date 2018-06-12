7408d162a8e62039852e77a0ca315d9f09f4190f
Niyas Raphy <niyas@cybrosys.in>
2018-06-12 17:27:31 +0530

    [ADD] Initial commit

---
+++ education_theme/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#    A part of Educational ERP Project <https://www.educationalerp.com>
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2018-TODAY Cybrosys Technologies (<https://www.cybrosys.com>).
+#    Author: Avinash Nk (<https://www.cybrosys.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###################################################################################
+{
+    'name': 'Educational ERP Theme',
+    'version': '11.0.1.0.0',
+    'summary': """Insight to Educational ERP""",
+    'description': """In trio combination of red, green and yellow Educational ERP, 
+    opens the new gate way for management""",
+    'category': 'Theme',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.educationalerp.com",
+    'depends': ['web'],
+    'data': [
+        'views/education_theme.xml',
+    ],
+    'demo': [],
+    'images': [
+        'static/description/banner.jpg',
+        'static/description/banner.png'
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ education_theme/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module education_theme
+
+#### 12.06.2018
+#### Version 11.0.1.0.0
+##### ADD
+- Initial commit for Educational ERP Project
