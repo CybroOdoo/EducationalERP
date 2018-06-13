15285c473c944411076ec445661e2b972fa94c6c
Niyas Raphy <niyas@cybrosys.in>
2018-06-13 14:34:07 +0530

    [UPDT] Banner

--- education_theme/__manifest__.py
+++ education_theme/__manifest__.py
@@ -37,8 +37,8 @@
     ],
     'demo': [],
     'images': [
-        'static/description/back_banner.jpg',
-        'static/description/front_banner.png'
+        'static/description/back_banner.png',
+        'static/description/front_banner.jpg'
     ],
     'license': 'AGPL-3',
     'installable': True,
