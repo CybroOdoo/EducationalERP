3121fb0ef7909a4093a33b438c00be1538e7e838
Niyas Raphy <niyas@cybrosys.in>
2018-06-13 09:13:10 +0530

    [UPTD] Access Rights

--- education_core/__manifest__.py
+++ education_core/__manifest__.py
@@ -56,7 +56,6 @@
         'views/education_amenities.xml',
         'reports/report.xml',
         'reports/student_id_card.xml',
-        # 'reports/student_application_report.xml',
         'reports/faculty_id_card.xml',
     ],
     'demo': [
